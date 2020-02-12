2612270eeaa463392525ed654d8721c050cc1123
kelly macagi <kelly.macagi@br.ey.com>
2020-02-12 17:08:08 -0300

    Aula 10 e exercícios.

---
+++ src/com/kelly/basicJava/aula10/variaveis.java
@@ -0,0 +1,31 @@
+package com.kelly.basicJava.aula10;
+
+public class variaveis {
+    public static void main(String[] args) {
+
+        //convencao java
+        int idade;
+        String nome = "Kelly";
+        String nomeDoMeuPet  = "Lulu";
+        String ano2020 = "2020";
+
+        //nao convencao java
+        int _idade;
+        int $idade;
+        String nome_do_meu_pet;
+        String NomeDoMeuPet;
+
+        // ma pratica
+        int a = 10;
+        String b = "Kelly1";
+
+        idade = 300;
+
+        System.out.println("Idade = " + idade);
+        System.out.println("Nome = " + nome);
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
